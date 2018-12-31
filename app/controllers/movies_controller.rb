9702b59ab270d55487622bbf967e7fd0b8852a1c
programoo <prakarnwongsanit@gmail.com>
2018-12-31 18:56:40 +0700

    Add show page

--- app/controllers/movies_controller.rb
+++ app/controllers/movies_controller.rb
@@ -1,7 +1,15 @@
 class MoviesController < ApplicationController
-
+  before_action :set_movie
   def index
     @movies = Movie.page(params[:page]).per(12)
   end
 
+  def show
+    @movie
+  end
+
+  private
+    def set_movie
+      @movie = Movie.find(params[:id])
+    end
 end
